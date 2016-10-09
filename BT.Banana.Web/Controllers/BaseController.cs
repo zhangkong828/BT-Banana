aa468667ba52d6d9625e86a0337558a92d7d2566
ZhangKong <602488225@qq.com>
2016-10-10 01:38:22 +0800

    多语言 本地化

---
+++ BT.Banana.Web/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using BT.Banana.Web.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT.Banana.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            string cultureName = null;
+
+            //先试图获取cookie中的语言设置，如果没有，则使用浏览器中带有的语言设置
+            var cultureCookie = Request.Cookies["_culture"];
+            if (cultureCookie != null)
+                cultureName = cultureCookie.Value;
+            else
+                cultureName = Request.UserLanguages != null && Request.UserLanguages.Length > 0 ?
+                Request.UserLanguages[0] : null;
+
+            // 验证，如果为空或者不在当前本地化内，则使用默认语言
+            cultureName = CultureHelper.GetImplementedCulture(cultureName); 
+
+                       
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cultureName);
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+
+            return base.BeginExecuteCore(callback, state);
+        }
+    }
+}
---
+++ BT.Banana.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BT.Banana.Web.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BT.Banana.Web.Controllers
+{
+    public class HomeController : BaseController
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 设置语言
+        /// </summary>
+        public ActionResult SetLang()
+        {
+            var culture = RequestHelper.GetValue("lang");
+            culture = CultureHelper.GetImplementedCulture(culture);
+            var cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }
+    }
+}
